d89f66d4044147652c092de13aea0de6e3b1a74b
leonard0-b <leonardo.bongianni@gmail.com>
2021-05-18 16:27:54 +0200

    Esercizio Concluso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,8 @@ Magnifici Filmoni
             <div class="title">{{$movie->title}}</div>
             <div class="original_title">{{$movie->original_title}}</div>
             <div class="nationality">{{$movie->nationality}}</div>
-            <div class="date">{{$movie->date}}</div>
-            <div class="vote">{{$movie->vote}}</div>
+            <div class="date">{{date('Y', strtotime($movie->date))}}</div>
+            <div class="vote">{{$movie->vote}}/10</div>
         </div>
     @endforeach
 </div>
